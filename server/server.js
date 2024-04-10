7b748c36777573df200508e724c58bf04bba5b4c
Ola Ekdahl <ola@ciracon.com>
2024-04-10 10:49:24 -0700

    added list of cards showing socks. to home component.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
 
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
@@ -16,6 +18,10 @@ app.get('/api/socks', async (_req, res) => {
         const client = await MongoClient.connect(url);
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
+        /**
+         * Retrieves all socks from the collection.
+         * @returns {Promise<Array>} A promise that resolves to an array of socks.
+         */
         const socks = await collection.find({}).toArray();
         res.json(socks);
     } catch (err) {
