101be54780eaaabf2af4307f5d81de07c46c8970
Ola Ekdahl <ola@ciracon.com>
2024-04-10 10:56:46 -0700

    added pager to to the api.

--- server/server.js
+++ server/server.js
@@ -29,6 +29,28 @@ app.get('/api/socks', async (_req, res) => {
         res.status(500).send('Hmm, something doesn\'t smell right... Error fetching socks');
     }
 });
+
+app.get('/api/socks/:page/:limit', async (req, res) => {
+    try {
+        let { page, limit } = req.params;
+        limit = +limit; // The + converts limit from a string to integer.
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        /**
+         * Retrieves a specific page of socks from the collection.
+         * @param {number} page - The page number.
+         * @param {number} limit - The number of records to display on the page.
+         * @returns {Promise<Array>} A promise that resolves to an array of socks.
+         */
+        const socks = await collection.find({}).skip((page - 1) * limit).limit(limit).toArray();
+        res.json(socks);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error fetching socks');
+    }
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
