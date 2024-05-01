62ef9ff594f9c2707d17de30574e1c6197bb13d1
Ola Ekdahl <ola@ciracon.com>
2024-05-01 07:01:34 -0700

    changed number of socks back to 50

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,51 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 /**
  * Search component for searching socks by color.
  *
  */
-const Search = ({onHandleSetData}) => {
-    const [searchTerm, setSearchTerm] = useState('');
+const Search = ({ onHandleSetData }) => {
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        fetch(`${import.meta.env.VITE_SOCKS_API_URL}/search`, {
-            method: 'POST',
-            body: JSON.stringify({ searchTerm }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                // Handle the response data
-                onHandleSetData(data);
-            })
-            .catch((error) => {
-                // Handle any errors
-                console.error(error);
-            });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`${import.meta.env.VITE_SOCKS_API_URL}/search`, {
+      method: "POST",
+      body: JSON.stringify({ searchTerm }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data
+        onHandleSetData(data);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+      });
+  };
 
-    const handleChange = (e) => {
-        setSearchTerm(e.target.value);
-    };
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
-    return (
-        <form className="d-flex" role="search" onSubmit={handleSubmit}>
-            <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Color"
-                aria-label="Search"
-                value={searchTerm}
-                onChange={handleChange}
-            />
-            <button className="btn btn-outline-success" type="submit">
-                Search
-            </button>
-        </form>
-    );
+  return (
+    <form className="d-flex" role="search" onSubmit={handleSubmit}>
+      <input
+        className="form-control me-2"
+        type="search"
+        placeholder="Color"
+        aria-label="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <button className="btn btn-outline-success" type="submit">
+        Search
+      </button>
+    </form>
+  );
 };
 
 export default Search;
